fcd7adb5b1b3faf3019039d71ba7dc3b054b21f6
julika-77 <julika-77@list.ru>
2016-07-22 00:21:40 +0300

    Работа с холстом tkinter

--- catch_the_ball/game_canvas.py
+++ catch_the_ball/game_canvas.py
@@ -12,12 +12,13 @@ def paint(event):
     canvas.coords(line,0,0,event.x,event.y)
 root=tkinter.Tk()
 
-canvas = tkinter.Canvas(root)
+canvas = tkinter.Canvas(root, background='white',width=400,height=400)
 canvas.bind("<Motion>",paint)
 canvas.pack()
 
 line = canvas.create_line(0,0,10,10)
-
+for i in range(10):
+    oval=canvas.create_oval(2+i*40,2+i*40,i*40+30,i*40+30,width=2,fill='green')
 
 root.mainloop()
 print("Эта строка будет достигнута только при выходе из приложения.")
